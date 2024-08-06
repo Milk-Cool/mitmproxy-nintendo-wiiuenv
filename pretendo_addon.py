03b7b4956762720b4757c36dfc0b4cdcbf362a06
Milk-Cool <milan-pl@mail.ru>
2024-08-06 15:27:46 +0300

    test

--- pretendo_addon.py
+++ pretendo_addon.py
@@ -1,7 +1,35 @@
 from mitmproxy import http, ctx
+from os.path import expanduser
+import os
+
+
+DEVICE_ID = None
+SERIAL_NUMBER = None
+SYSTEM_VERSION = None
+REGION_ID = None
+COUNTRY_NAME = None
+LANGUAGE = None
+USERNAME = None
+PASSWORD = None
+CERT = None
 
 
 class PretendoAddon:
+    def running(_self):
+        ctx.log.info("Building Inkay...")
+        certf = open(expanduser("~/.mitmproxy/mitmproxy-ca-cert.pem"), "r")
+        cert = certf.read()
+        certf.close()
+
+        inkaycertf = open(os.path.join(os.getcwd(), "Inkay", "data", "ca.pem"), "w")
+        inkaycertf.write(cert)
+        inkaycertf.close()
+
+        os.system("docker build Inkay -t inkay-build")
+        os.system(f"docker run -it --rm -v {os.path.join(os.getcwd(), "Inkay")}:/app -w /app inkay-build")
+
+        ctx.log.info("Built Inkay! Built module can be found at Inkay/Inkay-pretendo.wps")
+    
     def load(self, loader) -> None:
         loader.add_option(
             name="pretendo_redirect",
@@ -32,6 +60,34 @@ class PretendoAddon:
         )
 
     def request(self, flow: http.HTTPFlow) -> None:
+        global DEVICE_ID, SERIAL_NUMBER, SYSTEM_VERSION
+        global REGION_ID, COUNTRY_NAME, LANGUAGE
+        global CERT, USERNAME, PASSWORD
+
+        h = flow.request.headers
+        if "X-Nintendo-Device-ID" in h:
+            DEVICE_ID = h["X-Nintendo-Device-ID"]
+        if "X-Nintendo-Serial-Number" in h:
+            SERIAL_NUMBER = h["X-Nintendo-Serial-Number"]
+        if "X-Nintendo-System-Version" in h:
+            SYSTEM_VERSION = hex(int(h["X-Nintendo-System-Version"], 16))[2:]
+        if "X-Nintendo-Region" in h:
+            REGION_ID = int(h["X-Nintendo-Region"])
+        if "X-Nintendo-Country" in h:
+            COUNTRY_NAME = h["X-Nintendo-Country"]
+        if "Accept-Language" in h:
+            LANGUAGE = h["Accept-Language"]
+        if "X-Nintendo-Device-Cert" in h:
+            CERT = h["X-Nintendo-Device-Cert"]
+
+        b = flow.request.urlencoded_form
+        if len(b.fields) == 0:
+            return
+        if "user_id" in b:
+            USERNAME = b["user_id"]
+        if "password" in b:
+            PASSWORD = b["password"]
+        
         if ctx.options.pretendo_redirect:
             if "nintendo.net" in flow.request.pretty_host:
                 flow.request.host = flow.request.pretty_host.replace(
@@ -54,6 +110,47 @@ class PretendoAddon:
 
                 if ctx.options.pretendo_http:
                     flow.request.scheme = "http"
+
+    def done(_self):
+        global DEVICE_ID, SERIAL_NUMBER, SYSTEM_VERSION
+        global REGION_ID, COUNTRY_NAME, LANGUAGE
+        global CERT, USERNAME, PASSWORD
+
+        env = open("wiiu.env", "w")
+        env.write(f"""DEVICE_ID={DEVICE_ID}
+SERIAL_NUMBER={SERIAL_NUMBER}
+SYSTEM_VERSION={SYSTEM_VERSION}
+REGION_ID={REGION_ID}
+COUNTRY_NAME={COUNTRY_NAME}
+LANGUAGE={LANGUAGE}
+CERT={CERT}
+USERNAME={USERNAME}
+PASSWORD={PASSWORD}""")
+        env.close()
+
+        envwin = open("wiiu.env", "w")
+        envwin.write(f"""set DEVICE_ID={DEVICE_ID}
+set SERIAL_NUMBER={SERIAL_NUMBER}
+set SYSTEM_VERSION={SYSTEM_VERSION}
+set REGION_ID={REGION_ID}
+set COUNTRY_NAME={COUNTRY_NAME}
+set LANGUAGE={LANGUAGE}
+set CERT={CERT}
+set USERNAME={USERNAME}
+set PASSWORD={PASSWORD}""")
+        envwin.close()
+
+        envlinux = open("wiiu.env", "w")
+        envlinux.write(f"""export DEVICE_ID={DEVICE_ID}
+export SERIAL_NUMBER={SERIAL_NUMBER}
+export SYSTEM_VERSION={SYSTEM_VERSION}
+export REGION_ID={REGION_ID}
+export COUNTRY_NAME={COUNTRY_NAME}
+export LANGUAGE={LANGUAGE}
+export CERT={CERT}
+export USERNAME={USERNAME}
+export PASSWORD={PASSWORD}""")
+        envlinux.close()
 
 
 addons = [PretendoAddon()]
